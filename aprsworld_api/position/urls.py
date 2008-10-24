cf0572d1c050179543fa8ef7560672ad8fc47fe3
Adam Fast <adamfast@gmail.com>
2008-10-24 01:08:36 +0000

    Committed the wrong version of urls/views for positions. This one is far more flexible

--- aprsworld_api/position/urls.py
+++ aprsworld_api/position/urls.py
@@ -1,5 +1,12 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^(?P<callsign>[-\w]+)/(?P<number_requested>\d+)', 'aprsworld_api.position.views.station_info'),
+    # latest - will only return one record
+    # can pass a ?format=<serialization format> to get it in your preferred format. yaml, json, xml
+    url(r'^latest/(?P<callsign>[-\w]+)/$', 'aprsworld_api.position.views.station_latest_position'),
+
+    # all - will return 10 unless you pass in the (optional) number_requested parameter
+    # can pass a ?format=<serialization format> to get it in your preferred format. yaml, json, xml
+    url(r'^(?P<callsign>[-\w]+)/(?P<number_requested>\d+)/$', 'aprsworld_api.position.views.station_positions'),
+    url(r'^(?P<callsign>[-\w]+)/$', 'aprsworld_api.position.views.station_positions'),
 )
--- aprsworld_api/position/views.py
+++ aprsworld_api/position/views.py
@@ -1,7 +1,31 @@
 from django.core import serializers
+from django.http import HttpResponse
 from models import Position, LastPosition
 
-def station_info(callsign, number_requested):
-    print('Call: *%s*, Number: *%s*' % (callsign, number_requested))
-    data = Position.objects.filter(callsign__startswith=callsign)[number_requested]
-    return HttpResponse(serializers.serialize("xml", data))
+def station_positions(request, callsign, number_requested=10):
+    data = Position.objects.filter(source__startswith=callsign)[:number_requested]
+
+    # handle different formats, but make sure it's something valid
+    if request.GET['format'] == 'xml':
+        format = 'xml'
+    elif request.GET['format'] == 'json':
+        format = 'json'
+    else:
+        format = 'yaml'
+
+    return HttpResponse(serializers.serialize(format, data))
+
+def station_latest_position(request, callsign):
+    data = LastPosition.objects.filter(source__startswith=callsign)
+    if data:
+        # handle different formats, but make sure it's something valid
+        if request.GET['format'] == 'xml':
+            format = 'xml'
+        elif request.GET['format'] == 'json':
+            format = 'json'
+        else:
+            format = 'yaml'
+
+        return HttpResponse(serializers.serialize(format, data))
+    else:
+        return HttpResponse("")
